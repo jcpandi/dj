6fd3bd637105a5cd2b884e83617d02b8f505fd0b
JC Pandi <jcpandi@gmail.com>
2021-09-07 02:12:34 +0800

    menu-adjustment

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -7,8 +7,8 @@ const Navbar = () => {
       <div className="logo">
         <Image src="/logo.png" alt="site logo" width={128} height={77} />
       </div>
-      <Link href="/"><a>Home</a></Link>
-      <Link href="/about"><a>About</a></Link>
+      <Link href="/"><a>Home</a></Link>|
+      <Link href="/about"><a>About</a></Link>|
       <Link href="/ninjas/"><a>Ninja Listing</a></Link>
     </nav>
   );
