000b9c3d866b1ed5b9b29ca9aadba36392d011b7
JC Pandi <jcpandi@gmail.com>
2021-09-07 02:06:11 +0800

    initial

---
+++ pages/ninjas/[id].js
@@ -0,0 +1,39 @@
+export const getStaticPaths = async () => {
+  const res = await fetch('https://fakestoreapi.com/products/');
+  const data = await res.json();
+
+  // map data to an array of path objects with params (id)
+  const paths = data.map(ninja => {
+    return {
+      params: { id: ninja.id.toString() }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch('https://fakestoreapi.com/products/' + id);
+  const data = await res.json();
+
+  return {
+    props: { ninja: data }
+  }
+}
+
+const Details = ({ ninja }) => {
+  return (
+    <div>
+      <h1>{ ninja.title }</h1>
+      <p>{ ninja.description }</p>
+      <p>{ ninja.price }</p>
+     
+    </div>
+  );
+}
+
+export default Details;
---
+++ pages/ninjas/index.js
@@ -0,0 +1,32 @@
+import styles from '../../styles/Ninjas.module.css'
+import Link from 'next/link'
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://fakestoreapi.com/products');
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data }
+  }
+}
+
+const Ninjas = ({ ninjas }) => {
+  // console.log(ninjas)
+
+  return (
+    <div>
+      <h1>All Ninjas</h1>
+      {ninjas.map(ninja => (
+        <Link href={'/ninjas/' + ninja.id} key={ninja.id}>
+          <a className={styles.single}>
+          <h3>{ ninja.title }</h3>
+            <h3>{ ninja.description }</h3>
+            <h3>{ ninja.price }</h3>
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
+}
+ 
+export default Ninjas;
