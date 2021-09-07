0b50f6b6239e4a31404d462535c289d2c2792dbd
JC Pandi <jcpandi@gmail.com>
2021-09-08 01:49:08 +0800

    add space

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -7,8 +7,8 @@ const Navbar = () => {
       <div className="logo">
         <Image src="/logo.png" alt="site logo" width={128} height={77} />
       </div>
-      <Link href="/"><a>Home</a></Link>|
-      <Link href="/about"><a>About</a></Link>|
+      <Link href="/"><a>Home</a></Link>{" "} | 
+      <Link href="/about"><a>About</a></Link>{" "} | 
       <Link href="/products/"><a>Products</a></Link>
     </nav>
   );
