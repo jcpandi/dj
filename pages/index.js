cf0a0f85ce5d98d25c4639849e871978d8f231fe
jcpandi <jcpandi@gmail.com>
2021-11-17 12:29:40 +0800

    change button link

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <h1 className={styles.title}>Homepage</h1>
         <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
         <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-        <Link href="/ninjas/">
+        <Link href="/products/">
           <a className={styles.btn}>List of Products</a>
         </Link>
       </div>
